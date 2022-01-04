6529c2400e6a7f48aa9fb5bdf820d7de5e23c9a1
thien137 <thienle1942@gmail.com>
2022-01-04 15:51:57 -0800

    progress

--- main.cpp
+++ main.cpp
@@ -1,3 +1,32 @@
+// Python Interpreter and REPL
+
+#include <iostream>
+#include <exception>
+#include <csignal>
+#include <string>
+#include "headers/python_exceptions.h"
+#include "headers/python_tokens.h"
+
 int main(int argc, char *argv[]) {
-  return 0;
+    std::signal(SIGINT, keyboard_interrupt_handler);
+    TokenStream ts {std::cin};
+    while (true) {
+        std::cout << ">>> ";
+        for (const Token& t : ts.tokenize_next_line(false)) {
+            switch (t.id) {
+                case TokenID::Operator:
+                case TokenID::String:
+                case TokenID::WhiteSpace:
+                case TokenID::Punctuation:
+                case TokenID::Identifier:
+                case TokenID::Comment:
+                    std::cout << t.string_value << std::endl;
+                    break;
+                case TokenID::Number:
+                    std::cout << t.double_value << std::endl;
+                    break;
+            }
+        }
+    }
+    return 0;
 }
